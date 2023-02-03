dbff2ef93c4dd15f942d15dab09e7ddbc6275c23
Rob Newport <robnewport@gmail.com>
2023-02-03 14:57:15 +1100

    Update TemporalBinning.m
    Added better descriptions and tightened up code

--- SoftMatchToolbox/TemporalBinning.m
+++ SoftMatchToolbox/TemporalBinning.m
@@ -1,10 +1,9 @@
 function fixation_sequence_cellarray = TemporalBinning(table, time_bin_size)
 
-
-time_counter = 0;
-
 fixation_sequence = [];
 fixation_sequence_cellarray = {};
+
+time_counter = 1;
 
     for row = 1:height(table)
 
@@ -12,29 +11,29 @@ fixation_sequence_cellarray = {};
         fixation_hilbert = table(row,:).h;
 
         if time_counter < time_bin_size 
-            % add hilbert to fixation sequence array
+            %% Add hilbert to fixation sequence array
             fixation_sequence = [fixation_sequence; fixation_hilbert time_counter];
             time_counter = time_counter + fixation_duration;
         else
 
-             % append fixation sequence array to cell array
+            %% Append fixation sequence array to cell array
             fixation_sequence_cellarray{end+1} = fixation_sequence;
-
-            last_hilbert = fixation_sequence(end, 1);            
+            
+            %% Truncate to last value
+            last_hilbert = fixation_sequence(end, 1);
+            remaining_time = time_counter - time_bin_size ;  
+            
+            %% Start new sequence
             fixation_sequence = [];
 
-            remaining_time = time_counter - time_bin_size ;       
             if remaining_time > 0 
-                % add last fixation to start of new fixation array
+                %% Add last fixation to start of new fixation array if time remains
                 fixation_sequence = [fixation_sequence; last_hilbert remaining_time];
+                time_counter = remaining_time;
+            else
+                %% Reset the counter if no more time remains
+                time_counter = 1;
             end
-
-            % append to fixation array with current fixation
-            fixation_sequence = [fixation_sequence; fixation_hilbert time_counter];
-
-            time_counter = remaining_time;
         end
-
     end
-
 end
